f17812d8db4bf4ed25086c3d3af868b60cbf41bf
Krzysztof Nasuta <krzysztof@nasuta.dev>
2026-07-04 16:51:37 +0200

    format: fix prettier

--- src/app/admin/users/types.ts
+++ src/app/admin/users/types.ts
@@ -6,11 +6,7 @@ export type User = UserDto;
 export type Apartment = ApartmentSummaryDto;
 
 export type EditMode =
-  | 'approve'
-  | 'change-status'
-  | 'assign-apartment'
-  | 'create-user'
-  | null;
+  'approve' | 'change-status' | 'assign-apartment' | 'create-user' | null;
 
 export interface NewUserFormData {
   email: string;
